48b7b01778e66aec04509574dbced71c223e477c
M. Mücahit TEZCAN <mmucahit.tezcan@gmail.com>
2021-07-18 01:15:50 +0300

    webpack config ✨

---
+++ webpack.config.js
@@ -0,0 +1,67 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const WorkboxWebpackPlugin = require('workbox-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+
+const isProduction = process.env.NODE_ENV == 'production'
+
+const config = {
+  entry: path.resolve(__dirname, 'src', 'index.tsx'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  mode: 'development',
+
+  module: {
+    rules: [
+      {
+        test: /\.[jt]sx?$/,
+        use: ['babel-loader'],
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
+        type: 'asset/inline',
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js', '.jsx'],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, './src/public/index.html'),
+    }),
+    new CleanWebpackPlugin(),
+  ],
+  devServer: {
+    contentBase: path.join(__dirname, './src'),
+    port: 3000,
+    hotOnly: true,
+    compress: true,
+    open: true,
+  },
+}
+
+module.exports = () => {
+  if (isProduction) {
+    config.mode = 'production'
+    config.plugins.push(new WorkboxWebpackPlugin.GenerateSW())
+  } else {
+    config.mode = 'development'
+  }
+  return config
+}
